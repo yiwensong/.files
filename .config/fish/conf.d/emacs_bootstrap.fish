7eb6ef35c63675928e46d2d8dad3ccc3c7c9aab6
Yiwen Song <songzgy@gmail.com>
2017-12-02 15:26:57 -0800

    Update emacs_bootstrap.fish

--- .config/fish/conf.d/emacs_bootstrap.fish
+++ .config/fish/conf.d/emacs_bootstrap.fish
@@ -1,4 +1,11 @@
-if status --is-login; and status --is-interactive; and not test -d ~/.emacs.d/emacs-for-python/
-    # Installs emacs-for-python via github
-    git clone --depth 1 https://github.com/gabrielelanaro/emacs-for-python.git ~/.emacs.d/emacs-for-python
+if status --is-login; and status --is-interactive
+    if not test -d ~/.emacs.d/emacs-for-python/
+        # Installs emacs-for-python via github
+        git clone --depth 1 https://github.com/gabrielelanaro/emacs-for-python.git ~/.emacs.d/emacs-for-python
+    end
+    
+    if not test -d ~/.emacs.d/emacs-color-theme-solarized/
+        # Installs emacs-color-theme-solarized
+        git clone --depth 1 https://github.com/sellout/emacs-color-theme-solarized.git ~/.emacs.d/emacs-color-theme-solarized
+    end
 end
