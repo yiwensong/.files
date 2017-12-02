0cf20e56d218d6af402fbcf870ef84bfaf4036b0
Yiwen Song <songzgy@gmail.com>
2017-12-02 11:37:52 -0800

    Create up.fish

---
+++ .config/fish/functions/up.fish
@@ -0,0 +1,21 @@
+# do you have updog?
+# what's updog?
+# just editing my up.fish
+
+function up -d 'cd up directory with int param'
+    if test (count $argv) -eq 0
+        # Default to `up 1` if no arguments provided
+        set levels 1
+    else
+        # + 0 is used to cast to int
+        set levels (math $argv[1] + 0)
+    end
+
+    for i in (seq $levels)
+        set path "../$path"
+    end
+
+    if test -n $path
+        cd $path
+    end
+end
