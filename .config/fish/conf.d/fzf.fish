4e01a915802ea1585c6694f9bc5363d4f74f5850
Yiwen Song <songzgy@gmail.com>
2017-12-02 11:32:26 -0800

    Create fzf.fish

---
+++ .config/fish/conf.d/fzf.fish
@@ -0,0 +1,5 @@
+if status --is-login; and status --is-interactive; and not type -q fzf
+    # Install fzf via vim-plug
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install --key-bindings --completion --update-rc --no-zsh
+end
