10147637ba166b0299c51cc135cf7c338803ae7d
Yiwen Song <songzgy@gmail.com>
2017-12-02 11:29:58 -0800

    Create 000-env.fish

---
+++ .config/fish/conf.d/000-env.fish
@@ -0,0 +1,11 @@
+# General environment variables
+
+if status --is-login
+    # Set default editor
+    set EDITOR "vim"
+    # Uncomment this if you hate yourself
+    # set EDITOR "emacs"
+    
+    # Add stuff to your PATH
+    set PATH $PATH
+end
