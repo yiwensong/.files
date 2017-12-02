2da8acfe834befd1ab75662e5796c2c02c0ca971
Yiwen Song <songzgy@gmail.com>
2017-12-02 11:34:03 -0800

    Update fzf.fish

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -1,5 +1,5 @@
 if status --is-login; and status --is-interactive; and not type -q fzf
-    # Install fzf via vim-plug
+    # Install fzf via github
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install --key-bindings --completion --update-rc --no-zsh
 end
