c07aa44c69c489b58b5537b70faa281dc5dceded
Yiwen Song <songzgy@gmail.com>
2017-12-16 15:55:39 -0800

    Update vim_bootstrap.fish

--- .config/fish/conf.d/vim_bootstrap.fish
+++ .config/fish/conf.d/vim_bootstrap.fish
@@ -1,3 +1,6 @@
 if status --is-login; and status --is-interactive
-    # TODO: make a vim bootstrapper
+    if not test -e ~/.vim/autoload/plug.vim
+        curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+                https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    end
 end
