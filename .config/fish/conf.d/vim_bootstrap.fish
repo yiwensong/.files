df7c2a1faad2fc7c95353395d4317696753b8ff0
Yiwen Song <songzgy@gmail.com>
2017-12-17 09:13:48 -0800

    Update vim_bootstrap.fish

--- .config/fish/conf.d/vim_bootstrap.fish
+++ .config/fish/conf.d/vim_bootstrap.fish
@@ -3,8 +3,13 @@ if status --is-login; and status --is-interactive
         curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
                 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     end
+    if not test -d ~/.vim/plugged/YouCompleteMe
+        set YCM_NEEDS_INSTALL 1
+    else
+        set YCM_NEEDS_INSTALL 0
+    end
     vim -c \'silent! PlugInstall\' -c \'qa\'
-    if not test ls
+    if test $YCM_NEEDS_INSTALL = "1"
         cd $HOME/.local/data
         curl -fLo $HOME/.local/data https://cmake.org/files/v3.10/cmake-3.10.1.tar.gz
         cd cmake-3.10.1
