9a40e7bb476299a0e4c79e1a62d29139a007531b
Yiwen Song <songzgy@gmail.com>
2017-12-16 16:15:26 -0800

    Update vim_bootstrap.fish

--- .config/fish/conf.d/vim_bootstrap.fish
+++ .config/fish/conf.d/vim_bootstrap.fish
@@ -4,6 +4,11 @@ if status --is-login; and status --is-interactive
                 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     end
     vim -c \'silent! PlugInstall\' -c \'qa\'
-    cd ~/.vim/plugged/YouCompleteMe/
+    cd $HOME/.local/data
+    curl -fLo $HOME/.local/data https://cmake.org/files/v3.10/cmake-3.10.1.tar.gz
+    cd cmake-3.10.1
+    ./bootstrap --prefix $HOME/.local/bin ; and make ; and make install
+    cd $HOME/.vim/plugged/YouCompleteMe/
     ./install.py
+    cd $HOME
 end
