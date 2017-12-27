bab66de7da35d8410d7c3a9d1388ef1c7d68470f
yiwen song <yiws@yelp.com>
2017-12-26 19:11:10 -0800

    removed emacs cause I don't use it anymore

--- .emacs.d/pymacs.el
+++
@@ -1,3 +0,0 @@
-(load-file "~/.emacs.d/emacs-for-python/epy-init.el")
-(epy-setup-ipython)
-(epy-setup-checker "pyflakes %f")
--- .emacs.d/whitespace.el
+++
@@ -1,6 +0,0 @@
-; Reconfigure whitespace-mode for day-to-day use
-(require 'whitespace)
-(setq whitespace-style '(face tabs space-before-tab tab-mark))
-(global-whitespace-mode t)
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-(setq-default whitespace-line-column 80)
