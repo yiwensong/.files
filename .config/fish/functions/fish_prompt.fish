2ba7f078ed00969d48f4f82f75c59b8f5e11cab0
yiwen song <yiws@yelp.com>
2017-12-28 00:54:09 -0800

    added some emojis

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,4 +1,5 @@
 function fish_prompt
+    set __prompt_status $status
     set sblue (set_color brblue)
     set syell (set_color yellow)
     set snorm (set_color normal)
@@ -8,7 +9,7 @@ function fish_prompt
     # echo $sblue"["$sbryl(whoami)$sblue"@"$sbrwt(hostname) $snorm(date +%H:%M:%S)$sblue] $sbryl(prompt_pwd)
     # echo $snorm"\$ "
 
-    set local_ids C02V20DBHTD8 YSONG121849
+    set local_ids C02V20DBHTD8
 
     # set user "$sbryl"(whoami)"$snorm"
     set islocal 0
@@ -28,5 +29,32 @@ function fish_prompt
     set tstamp "$snorm"(date +%H:%M:%S)"$snorm"
     set path "$sbryl"(prompt_pwd)"$snorm"
 
-    printf "%s[%s%s@%s %s%s] %s%s%s\n%s\$ \n" $sblue $user $sblue $host $tstamp $sblue $path $snorm
+    printf "%s[%s%s🌮 %s %s%s] %s%s%s\n%s" $sblue $user $sblue $host $tstamp $sblue $path $snorm
+    if test $__prompt_status -ne 0
+        __fail_emoji
+    else
+        __success_emoji
+    end
+end
+
+
+function __success_emoji -d "makes a success emoji"
+    set rand (random)
+    set big 32767
+    if test $rand -lt (echo $big"/5" | bc)
+        printf "👌  \n"
+    else if test $rand -lt (echo $big"*2/5" | bc)
+        printf "🙏  \n"
+    else if test $rand -lt (echo $big"*3/5" | bc)
+        printf "👍  \n"
+    else if test $rand -lt (echo $big"*4/5" | bc)
+        printf "🙌  \n"
+    else
+        printf "✌️  \n"
+    end
+end
+
+
+function __fail_emoji -d "makes a failure emoji"
+    printf "🔥  \n"
 end
