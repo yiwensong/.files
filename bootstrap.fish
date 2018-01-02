078c2bdada0c35baad31574da914fc4b1ad51d12
yiwen song <yiws@yelp.com>
2018-01-01 20:55:07 -0800

    made bootstrap less dangerous and refactored vimrc

--- bootstrap.fish
+++ bootstrap.fish
@@ -1,14 +1,30 @@
 #!/usr/bin/fish
 set confdir (pwd)"/"(dirname (status -f))
 
-# remove old configs
-rm -rf $HOME/.config/fish
-rm -rf $HOME/.vimrc
-rm -rf $HOME/.tmux.conf
+# move old configs
+set backup_no 0
+set backup_folder $HOME/.dot-files-backup.$backup_no
+while test -d $backup_folder
+    set backup_no (math $backup_no + 1)
+    set backup_folder $HOME/.dot-files-backup.$backup_no
+end
+mkdir $backup_folder
+
+if test -d $HOME/.config/fish
+    mv $HOME/.config/fish $backup_folder
+end
+
+if test -f $HOME/.vimrc
+    mv $HOME/.vimrc $backup_folder
+end
+
+if test -f $HOME/.tmux.conf
+    mv $HOME/.tmux.conf $backup_folder
+end
 
 # make a ~/.config folder if not there
 if not test -d $HOME/.config
-  mkdir $HOME/.config
+    mkdir $HOME/.config
 end
 
 # softlink them to this directory
