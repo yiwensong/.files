c5af320916352d4058b911231a59d85ed14bd91a
yiwen song <yiwensong.com@gmail.com>
2018-08-25 00:48:03 +0000

    add python virtualenv

---
+++ .config/fish/conf.d/python_venv.fish
@@ -0,0 +1,6 @@
+if status --is-login; and status --is-interactive
+    if not test -e ~/.venv/bin
+        virtualenv --python=python3 ~/.venv
+        ~/.venv/bin/pip3 install ipython
+    end
+end
