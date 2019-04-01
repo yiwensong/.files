6d49065f652de5787d2ae67d1c7f7b37281021d7
yiwen song <songzgy@gmail.com>
2019-04-01 02:08:54 -0700

    clean up bootstrapping code for compatibility

--- .config/fish/conf.d/python_venv.fish
+++ .config/fish/conf.d/python_venv.fish
@@ -1,6 +1,6 @@
 if status --is-login; and status --is-interactive
     if not test -e ~/.venv/bin
-        virtualenv --python=python3 ~/.venv
+        python3 -m virtualenv --python=python3 ~/.venv
         ~/.venv/bin/pip3 install ipython
     end
     pip3 install jedi
--- .config/fish/conf.d/vim_bootstrap.fish
+++ .config/fish/conf.d/vim_bootstrap.fish
@@ -1,32 +1,16 @@
-function _install_ycm
-    set WORKDIR (pwd)
-    if not test -d $HOME/.local/data
-      mkdir $HOME/.local/data
-    end
-    curl -fLo $HOME/.local/data/cmake-3.10.1.tar.gz https://cmake.org/files/v3.10/cmake-3.10.1.tar.gz
-    cd $HOME/.local/data/
-    tar -xvf cmake-3.10.1.tar.gz
-    cd $HOME/.local/data/cmake-3.10.1
-    ./bootstrap --prefix=$HOME/.local ; and make ; and make install
-    eval $HOME/.vim/plugged/YouCompleteMe/install.py --js-completer --clang-completer --system-libclang --go-completer
-    cd $WORKDIR
+head -n 100 $HOME/.vimrc > $HOME/.vimrc_bootstrap
+if not test -e ~/.vim/autoload/plug.vim
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 end
-
-if status --is-login; and status --is-interactive
-    head -n 100 $HOME/.vimrc > $HOME/.vimrc_bootstrap
-    if not test -e ~/.vim/autoload/plug.vim
-        curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-                https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    end
-    if type -q vim8
-        eval "vim8 -u $HOME/.vimrc_bootstrap -c 'silent! PlugInstall' -c 'qa'"
-    else
-        eval "vim -u $HOME/.vimrc_bootstrap -c 'silent! PlugInstall' -c 'qa'"
-    end
-    if not test -d ~/.vim/undo
-        mkdir ~/.vim/undo
-    end
-    if not test -d ~/.vim/swapfiles
-        mkdir ~/.vim/swapfiles
-    end
+if type -q vim8
+    eval "vim8 -u $HOME/.vimrc_bootstrap -c 'silent! PlugInstall' -c 'qa'"
+else
+    eval "vim -u $HOME/.vimrc_bootstrap -c 'silent! PlugInstall' -c 'qa'"
+end
+if not test -d ~/.vim/undo
+    mkdir ~/.vim/undo
+end
+if not test -d ~/.vim/swapfiles
+    mkdir ~/.vim/swapfiles
 end
