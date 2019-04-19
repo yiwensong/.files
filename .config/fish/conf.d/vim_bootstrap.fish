6dced6a037bec114e13890b7159a70198f5620ab
yiwen song <songzgy@gmail.com>
2019-04-19 13:22:39 -0700

    add some comments to plugins and made vimrc bootstrap longer

--- .config/fish/conf.d/vim_bootstrap.fish
+++ .config/fish/conf.d/vim_bootstrap.fish
@@ -1,5 +1,5 @@
 if status --is-login; and status --is-interactive
-    head -n 100 $HOME/.config/nvim/init.vim > $HOME/.config/nvim/init_bootstrap.vim
+    head -n 250 $HOME/.config/nvim/init.vim > $HOME/.config/nvim/init_bootstrap.vim
     if not test -e $HOME/.local/share/nvim/site/autoload/plug.vim
         curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
