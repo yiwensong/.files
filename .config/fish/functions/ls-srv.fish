0995533564eb4303824243bfe09a810e21d2a3bc
yiwen song <songzgy@gmail.com>
2019-10-25 01:25:37 +0000

    fix ls-srv

--- .config/fish/functions/ls-srv.fish
+++ .config/fish/functions/ls-srv.fish
@@ -1,3 +1,3 @@
 function ls-srv
-    sc ls-servers --map srfc39_fqdn $argv 2>&1 | rg 'i-.*\.' |  cut -f-1
+    sc ls-servers --map srfc39_fqdn $argv 2>&1 | rg 'i-.*\.' |  awk -F" " '{print $NF}'
 end
