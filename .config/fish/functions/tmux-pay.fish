d754adc02788da60b60b2b4c820751d5214366e5
yiwen song <songzgy@gmail.com>
2019-11-19 17:59:05 +0000

    improve tmux-proj and tmux-pay to be better inside tmux

--- .config/fish/functions/tmux-pay.fish
+++ .config/fish/functions/tmux-pay.fish
@@ -1,11 +1,17 @@
-function tmux_pay --description 'starts a tmux session for pay-server'
+function tmux-pay --description 'starts a tmux session for pay-server'
     set session_name pay-server
     set main_pane_name pay
     set side_pane_name "pay up"
 
+    set psdir $HOME/stripe/pay-server
+
     if tmux ls | rg $session_name
-        tmux a -t $session_name
-        exit 0
+        if test -z $TMUX
+            tmux attach -t $session_name
+        else
+            tmux switch-client -t $session_name
+        end
+        return
     end
 
     if tmux ls
@@ -18,12 +24,18 @@ function tmux_pay --description 'starts a tmux session for pay-server'
 
     echo $width x $height
 
-    tmux new-session -d -s $session_name -c $HOME/stripe/pay-server -x $width -y $height
+    tmux new-session -d -s $session_name -c $psdir -x $width -y $height
     tmux rename-window -t $session_name:1 $main_pane_name
-    tmux split-window -h -t $session_name:$main_pane_name.1
+    tmux split-window -h -t $session_name:$main_pane_name.1 -c $psdir
     tmux resize-pane -t $session_name:$main_pane_name.1 -x 100
 
-    tmux new-window -t $session_name -n $side_pane_name
-    tmux split-window -h -t $session_name:$side_pane_name.1
+    tmux new-window -t $session_name -n $side_pane_name -c $psdir
+    tmux split-window -h -t $session_name:$side_pane_name.1 -c $psdir
     tmux resize-pane -t $session_name:$side_pane_name.1 -x 100
+
+    if test -z $TMUX
+        tmux attach -t $session_name:$main_pane_name.1
+    else
+        tmux switch-client -t $session_name:$main_pane_name.1
+    end
 end
--- .config/fish/functions/tmux-proj.fish
+++ .config/fish/functions/tmux-proj.fish
@@ -1,9 +1,14 @@
 function tmux-proj --description 'starts a tmux session with sane defaults'
     set session_name $argv[1]
+    set session_path $HOME/stripe/$session_name
 
     if tmux ls | rg $session_name
-        tmux a -t $session_name
-        exit 0
+        if test -z $TMUX
+            tmux attach -t $session_name
+        else
+            tmux switch-client -t $session_name
+        end
+        return
     end
 
     if tmux ls
@@ -16,9 +21,14 @@ function tmux-proj --description 'starts a tmux session with sane defaults'
 
     echo $width x $height
 
-    tmux new-session -d -s $session_name -c $HOME/stripe/$session_name -x $width -y $height
+    tmux new-session -d -s $session_name -c $session_path -x $width -y $height
     tmux rename-window -t $session_name:1 $session_name
-    tmux split-window -h -t $session_name:$session_name.1
+    tmux split-window -h -t $session_name:$session_name.1 -c $session_path
     tmux resize-pane -t $session_name:$session_name.1 -x 100
-    tmux attach -t $session_name
+
+    if test -z $TMUX
+        tmux attach -t $session_name:$session_name.1
+    else
+        tmux switch-client -t $session_name:$session_name.1
+    end
 end
