91352beb918caf11894a1225737daa3e911e9d87
yiwen song <songzgy@gmail.com>
2020-03-14 01:49:04 +0000

    make ecr login a function instead

--- .config/fish/functions/ecr-login.fish
+++ .config/fish/functions/ecr-login.fish
@@ -1,4 +1,4 @@
-if status --is-login; and command -v sc
+function ecr-login -d 'logs you into ecr'
     sc env stripe.io --role admin
     set ecr_login_cmd (sc aws ecr get-login --no-include-email --region us-west-2 | string split " ")
     echo $ecr_login_cmd
