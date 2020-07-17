2f9f7dcfffd8703ff9d75777f05c34ba234b23f3
yiwen song <songzgy@gmail.com>
2020-07-17 23:00:15 +0000

    venv improvements

--- .config/fish/conf.d/python_venv.fish
+++ .config/fish/conf.d/python_venv.fish
@@ -1,6 +1,7 @@
 if status --is-login; and status --is-interactive
     if not test -e $HOME/.venv/bin
-        python3 -m virtualenv --python=python3 ~/.venv
+        /usr/local/bin/pip3 install virtualenv
+        /usr/local/bin/python3 -m virtualenv --python=python3 ~/.venv
         $HOME/.venv/bin/pip3 install ipython
     end
     $HOME/.venv/bin/pip3 install jedi
